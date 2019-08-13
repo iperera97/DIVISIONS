ed2635358bd410b10737270a802f4357186e6e93
waex <isurumahesh97@gmail.com>
2019-08-13 20:34:03 +0530

    city update controller done

--- city/urls.py
+++ city/urls.py
@@ -7,4 +7,5 @@ app_name = "city"
 urlpatterns = [
     re_path(r'^$', views.ListCity.as_view(), name="home"),
     re_path(r'^create/$', views.CreateCity.as_view(), name="create"),
+    re_path(r'^edit/(?P<pk>\d+)/$', views.UpdateCity.as_view(), name="edit"),
 ]
--- city/views.py
+++ city/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
@@ -7,10 +7,12 @@ from .forms import CityInitForm
 from django.contrib import messages
 
 
+# create city with model form
 class CreateCity(LoginRequiredMixin, View):
 
     TEMPLATE_NAME = "city/create.html"
 
+    # init form
     def get(self, request, *args, **kwargs):
 
         form = CityInitForm()
@@ -22,15 +24,18 @@ class CreateCity(LoginRequiredMixin, View):
 
         return render(request, self.TEMPLATE_NAME, parseData)
 
+    # pass form data to database and redirect
     def post(self, request, *args, **kwargs):
 
-        form = CityInitForm(request.POST)
+        form = CityInitForm(request.POST, request.FILES)
 
         # if form is valid
         if form.is_valid():
 
+            # save db
             form.save()
 
+            # flash messages
             messages.success(request, " {} City Added ".format(
                 request.POST.get("englishName")))
 
@@ -44,16 +49,46 @@ class CreateCity(LoginRequiredMixin, View):
         return render(request, self.TEMPLATE_NAME, parseData)
 
 
+# update city data through model form
 class UpdateCity(LoginRequiredMixin, View):
 
     TEMPLATE_NAME = "city/edit.html"
 
+    # get relevent object details
+    # bound model form
     def get(self, request, *args, **kwargs):
 
-        return render(request, self.TEMPLATE_NAME)
+        city = get_object_or_404(City, pk=kwargs.get('pk'))
+        form = CityInitForm(instance=city)
+
+        parseData = {
+            'title': 'Update {}'.format(city.englishName),
+            'form': form
+        }
+
+        return render(request, self.TEMPLATE_NAME, parseData)
 
     def post(self, request, *args, **kwargs):
-        pass
+
+        city = get_object_or_404(City, pk=kwargs.get('pk'))
+        form = CityInitForm(request.POST, instance=city)
+
+        parseData = {
+            'form': form,
+            'title': 'Update {}'.format(city.englishName)
+        }
+
+        if form.is_valid():
+            # save record
+            form.save()
+
+            messages.success(request, " {} City Added ".format(
+                request.POST.get("englishName")))
+
+            return redirect("city:home")
+
+        else:
+            return render(request, self.TEMPLATE_NAME, parseData)
 
 
 class RemoveCity(LoginRequiredMixin, View):
@@ -63,6 +98,14 @@ class RemoveCity(LoginRequiredMixin, View):
 class ListCity(LoginRequiredMixin, ListView):
 
     queryset = City.objects.all()
+    queryset = queryset.select_related('district')
+    queryset = queryset.values(
+        'pk',
+        'sinhalaName',
+        'englishName',
+        'district_id',
+        'district__englishName')
+
     template_name = "city/list.html"
 
     def get_context_data(self, *args, **kwargs):
